531782c15d87dca7d265ff07d41479bbcf4c52fc
abiralm <abiral.sayami@gmail.com>
2026-04-01 21:42:38 +0545

    add(products): added models to admin.py

--- backend/products/admin.py
+++ backend/products/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
-
+from .models import Category, Product
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    #populates slug on basis of field
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name','slug','category','price','available','created','updated']
+    list_filter = ['available', 'created', 'updated']
+    list_editable = ['price', 'available']
+    prepopulated_fields = {'slug': ('name',)}
+
+
