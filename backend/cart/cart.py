3993caae953d15dcdf939d1bcfe0c1dd5aa4e402
abiralm <abiral.sayami@gmail.com>
2026-04-04 16:11:33 +0545

    add(cart): added cart class logic

---
+++ backend/cart/cart.py
@@ -0,0 +1,70 @@
+from products.models import Product
+from django.conf import settings 
+from decimal import Decimal #creates deciaml obj
+
+class Cart:
+    def __init__(self,request):
+        #initialzes the cart
+        self.session = request.session
+        cart= self.session.get(settings.CART_SESSION_ID)
+        if not cart:
+            cart = self.session[settings.CART_SESSION_ID] ={}
+        self.cart= cart
+    
+    def add_to_cart(self, product, quantity=1, override_quantity=False):
+        """Add a product to the cart or update its quantity."""
+        product_id = str(product.id)
+
+        if product_id not in self.cart:
+            self.cart[product_id] = {
+                #'name': product.name,
+                'quantity': 0,
+                'price': str(product.price)
+            }
+        
+        if override_quantity:
+            self.cart[product_id]['quantity'] = quantity
+        else:
+            self.cart[product_id]['quantity'] += quantity
+        self.save()
+
+    def remove_from_cart(self,product,quantity):
+        product_id = str(product.id)
+
+        if product_id in self.cart:
+            self.cart[product_id]['quantity'] -= quantity
+
+            if self.cart[product_id]['quantity'] <= 0:
+                del self.cart[product_id]
+        self.save()
+
+    def clear_cart(self):
+        del self.session[settings.CART_SESSION_ID]
+        self.save()
+
+    def __len__(self):
+        return sum(item['quantity'] for item in self.cart.values())
+    
+    def get_total_price(self):
+        return sum(
+            Decimal(item['price']) * item['quantity']
+            for item in self.cart.values()
+        )
+    
+    def __iter__(self):
+        """Iterate over items and attach product objects from the database."""
+        product_ids = self.cart.keys()
+        products = Product.objects.filter(id__in=product_ids)
+        cart = self.cart.copy()
+
+        for product in products:
+            cart[str(product.id)]['product'] = product
+        
+        for item in cart.values():
+            item['price'] = Decimal(item['price'])
+            item['total_price'] = item['price'] * item['quantity']
+            yield item
+    
+    def save(self):
+        # mark the session as "modified" to make sure it gets saved
+        self.session.modified = True
