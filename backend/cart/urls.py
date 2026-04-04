133bd13944a17626986617ccc0a5de3fec4e92b8
abiralm <abiral.sayami@gmail.com>
2026-04-04 19:18:14 +0545

    fix(cart): fixed cart urls

--- backend/cart/urls.py
+++ backend/cart/urls.py
@@ -5,7 +5,7 @@ app_name ='cart'
 
 urlpatterns = [
     path('', view_cart, name='cart_detail'),
-    path('add/<int:product_id>/', add_to_cart, name='add_to_cart'),
-    path('remove/<int:product_id>/', remove_from_cart, name='remove_from_cart'),
-    path('clear/', clear_cart, name='clear_cart'),
+    path('add', add_to_cart, name='add_to_cart'),
+    path('remove', remove_from_cart, name='remove_from_cart'),
+    path('clear', clear_cart, name='clear_cart'),
 ]
