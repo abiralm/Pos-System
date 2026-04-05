c6d975b14e04bfb86b6925522834873c1c44a31b
abiralm <abiral.sayami@gmail.com>
2026-04-05 18:42:28 +0545

    add(orders): added orders, orderitem models

--- backend/orders/models.py
+++ backend/orders/models.py
@@ -1,3 +1,48 @@
 from django.db import models
+from products.models import Product
 
-# Create your models here.
+STATUS_CHOICES = [
+    ('paid','Paid'),
+    ('pending','Pending'),
+    ('cancelled','Cancelled')
+]
+
+
+class Order( models.Model):
+    customer_name= models.CharField(max_length=50)
+    email = models.EmailField()
+    #may ned to add phone address fields
+    status  = models.CharField(max_length=20, choices=STATUS_CHOICES, default='pending')
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True) 
+    discount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    tax = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+
+    class Meta:
+        ordering = ['-created']
+        indexes = [
+            models.Index(fields=['created'])
+        ]
+    
+    def get_sub_total(self):
+        return sum(item.get_cost() for item in self.items.all())
+    
+    def get_grand_total(self):
+        return self.get_subtotal() - self.discount + self.tax
+    
+    def __str__(self):
+        return 'Order {self.id}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order,related_name='order',on_delete=models.CASCADE)
+    product =models.ForeignKey(Product, related_name='order_items', on_delete=models.CASCADE)
+    #product_name = models.CharField(max_length=255)  in case product is deleted
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    quantity = models.PositiveIntegerField(default=1)
+
+    def __str__(self):
+        return str(self.id)
+
+    def get_cost(self):
+        return self.price * self.quantity
+
