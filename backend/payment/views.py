a74484f8e730f9fbd64c1ac0f7f191310acde95a
abiralm <abiral.sayami@gmail.com>
2026-04-19 00:18:38 +0545

    add(payments): added initial stripe payment configuration

--- backend/payment/views.py
+++ backend/payment/views.py
@@ -1,10 +1,16 @@
-from rest_framework.decorators import APIView
+from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-
+from config import settings
 from .serializers import PaymentSerializer
 from .models import Payment
 from orders.models import Order
+import stripe
+
+#stripe instance
+stripe.api_key = settings.STRIPE_SECRET_KEY
+stripe.api_version = settings.STRIPE_API_VERSION
+
 
 class PaymentView(APIView):
     def post(self,request):
@@ -13,23 +19,49 @@ class PaymentView(APIView):
 
         order_id = serializer.validated_data['order_id']
         method = serializer.validated_data['method']
-
         order = Order.objects.get(id=order_id)
+
         if order.status == 'paid':
             return Response({"error": "Already paid"}, status=400)
-        
+                    
+
+        #stripe session data
+        session_data = stripe.checkout.Session.create(
+            mode='payment',
+            client_reference_id=order_id,
+            success_url='http://localhost:8000/payment/success/',
+            cancel_url='http://localhost:8000/payment/cancel/',
+            payment_method_types=['card'],
+            line_items=[
+                {
+                    'price_data': {
+                        'unit_amount': int(order.get_grand_total() * 100),
+                        'currency': 'usd',
+                        'product_data': {
+                            'name': f'Order {order.id}',
+                        },
+                    },
+                    'quantity': 1,
+                }
+            ],
+            metadata={'order_id': order.id}
+        )
         payment = Payment.objects.create(
             order = order,
             method = method,
             amount = order.get_grand_total(),
-            status = 'completed'
+            status = 'pending'
         )
-        payment.save()
-
-        order.status = 'paid'
-        order.save()
 
         return Response({
-            "message": "Payment successful",
-            "payment_id": payment.id
-        },status=status.HTTP_200_OK)
+            "checkout_url": session_data.url
+        })
+        # payment.save()
+
+        # order.status = 'paid'
+        # order.save()
+
+        # return Response({
+        #     "message": "Payment successful",
+        #     "payment_id": payment.id
+        # },status=status.HTTP_200_OK)
