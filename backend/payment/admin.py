124503c130e5b80b172eb12174ae6a4b957dcd5c
abiralm <abiral.sayami@gmail.com>
2026-04-18 18:03:49 +0545

    add(payments): added payment model views,urls

--- backend/payment/admin.py
+++ backend/payment/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Payment
 
-# Register your models here.
+@admin.register(Payment)
+class PaymentAdmin(admin.ModelAdmin):
+    list_display = ['id', 'order', 'method', 'amount', 'status', 'created_at']
+    list_filter = ['status', 'method']
+    search_fields = ['order__id', 'stripe_id']
--- backend/payment/views.py
+++ backend/payment/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from rest_framework.decorators import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
-# Create your views here.
+from .serializers import PaymentSerializer
+from .models import Payment
+from orders.models import Order
+
+class PaymentView(APIView):
+    def post(self,request):
+        serializer = PaymentSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        order_id = serializer.validated_data['order_id']
+        method = serializer.validated_data['method']
+
+        order = Order.objects.get(id=order_id)
+        if order.status == 'paid':
+            return Response({"error": "Already paid"}, status=400)
+        
+        payment = Payment.objects.create(
+            order = order,
+            method = method,
+            amount = order.get_grand_total(),
+            status = 'completed'
+        )
+        payment.save()
+
+        order.status = 'paid'
+        order.save()
+
+        return Response({
+            "message": "Payment successful",
+            "payment_id": payment.id
+        },status=status.HTTP_200_OK)
