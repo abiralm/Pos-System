1fb2338038dbf1b823b3284e37a7f1d1c5982017
abiralm <abiral.sayami@gmail.com>
2026-05-02 13:07:41 +0545

    add(orders): invoice generation functionality

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import CheckoutView
+from .views import CheckoutView, generate_order_pdf
 app_name ='orders'
 
 urlpatterns = [
     path('checkout/', CheckoutView.as_view(), name='cart_detail'),
+    path('<int:order_id>/pdf/', generate_order_pdf, name='order_pdf')
 ]
--- backend/orders/views.py
+++ backend/orders/views.py
@@ -6,6 +6,13 @@ from cart.cart import Cart
 from .serializers import CheckoutSerializer
 from .services import create_order_from_cart
 
+from django.http import HttpResponse
+from django.template.loader import render_to_string
+from xhtml2pdf import pisa
+from django.shortcuts import get_object_or_404
+from .models import Order
+
+
 class CheckoutView(APIView):
     def post(self,request):
         cart = Cart(request)
@@ -26,4 +33,22 @@ class CheckoutView(APIView):
             "order_id": order.id,
             "total": order.get_grand_total()
         },status=status.HTTP_200_OK)
+
+
+def generate_order_pdf(request, order_id):
+    order = get_object_or_404(Order, id=order_id)
+
+    html = render_to_string('orders/order/pdf.html', {
+        'order': order
+    })
+
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = f'attachment; filename=order_{order.id}.pdf'
+
+    pisa_status = pisa.CreatePDF(html, dest=response)
+
+    if pisa_status.err:
+        return HttpResponse('Error generating PDF', status=500)
+
+    return response
 
