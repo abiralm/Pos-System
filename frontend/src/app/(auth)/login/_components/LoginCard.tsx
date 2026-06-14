a9bcd82c0bf1cc82f4cbe41b183f0474d0459d05
abiralm <abiral.sayami@gmail.com>
2026-06-14 19:24:43 +0545

    enhance(login):login page passwor visibility

--- frontend/src/app/(auth)/login/_components/LoginCard.tsx
+++ frontend/src/app/(auth)/login/_components/LoginCard.tsx
@@ -6,12 +6,19 @@ import {
     FieldLabel,
 } from "@/components/ui/field"
 import { Input } from "@/components/ui/input"
+import { Eye, EyeOff } from "lucide-react";
+import { useState } from "react";
 
 export function LoginCard() {
+    const [showPassword, setShowPassword] = useState(false);
+    const togglePasswordVisibility = () => {
+        setShowPassword(!showPassword)
+    }
+
     return (
         <div className="h-screen flex items-center justify-center  bg-gray-100">
             <div className="flex flex-col w-full max-w-md p-8 border-2 border-gray-600 rounded-lg shadow-lg">
-                
+
                 <div className="mb-8">
                     <h1 className="text-4xl font-bold mb-2">
                         Welcome Back!
@@ -21,19 +28,36 @@ export function LoginCard() {
                     </p>
                 </div>
 
-                <FieldGroup className="space-y-2">
+                <FieldGroup className="-space-y-1">
                     <Field>
                         <Input placeholder="Username" />
                     </Field>
 
                     <Field>
-                        <Input
-                            type="password"
-                            placeholder="Password"
-                        />
+                        <div className="relative">
+                            <Input
+                                type={showPassword ? "text" : "password"}
+                                placeholder="Password"
+                                className="pr-10"
+                            />
+
+                            {!showPassword ? (
+                                <EyeOff
+                                    size={18}
+                                    onClick={togglePasswordVisibility}
+                                    className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-muted-foreground"
+                                />
+                            ) : (
+                                <Eye
+                                    size={18}
+                                    onClick={togglePasswordVisibility}
+                                    className="absolute right-3 top-1/2 -translate-y-1/2 cursor-pointer text-muted-foreground"
+                                />
+                            )}
+                        </div>
                     </Field>
 
-                    <Button className="w-full">
+                    <Button className="w-full p-5">
                         Login
                     </Button>
                 </FieldGroup>
