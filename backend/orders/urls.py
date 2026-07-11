c324ef010cce27f8e929d546230b092ccc632177
abiralm <abiral.sayami@gmail.com>
2026-07-11 15:19:07 +0545

    add(orders):get orders backend added

--- backend/orders/urls.py
+++ backend/orders/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import CheckoutView, generate_order_pdf
+from .views import CheckoutView, generate_order_pdf, OrderListAPIView
 app_name ='orders'
 
 urlpatterns = [
+    path('', OrderListAPIView.as_view(), name='order_list'),
     path('checkout/', CheckoutView.as_view(), name='cart_detail'),
     path('<int:order_id>/pdf/', generate_order_pdf, name='order_pdf')
 ]
